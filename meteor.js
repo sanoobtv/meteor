132386755cf29ed384c8a6190e9ac30fe92a3a87
sanoob <sanoob.tv@gmail.com>
2016-04-01 16:21:21 +0000

    first

---
+++ meteor.js
@@ -0,0 +1,16 @@
+
+Resolutions = new Mongo.Collection('resolutions');
+
+if (Meteor.isClient) {
+
+Template.body.helpers({
+ 	 resolutions: function() { return Resolutions.find(); }
+ 
+});
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
