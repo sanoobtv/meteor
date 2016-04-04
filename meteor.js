78678c4a7bddee0eb1d8e55d35c0ea5a864e58c4
sanoob.tv <sanoob.tv@gmail.com>
2016-04-04 06:58:56 -0700

    update and delete

--- meteor.js
+++ meteor.js
@@ -5,29 +5,41 @@ if (Meteor.isClient) {
 
 Template.body.helpers({
  	 tasks: function() { return Tasks.find({},{sort: {createdAt: 1}}); }
- 
+
 });
 
 Template.body.events({
   'submit .newtask'(event){
-    
+
     //prevent default submit
     event.preventDefault();
-    
+
     //fetch value
     const target=event.target;
     const text= target.text.value;
-    
+
     //insertit to db
   Tasks.insert({
     text,
     createdAt: new Date(),
-    
+
   });
   target.text.value=' ';
   }
 })
+Template.task.events({
+'click .toggle-checked': function(){
 
+Tasks.update(this._id, {$set: {checked: !this.checked}})
+
+},
+
+  'click .delete': function(){
+    Tasks.remove(this._id);
+
+}
+
+});
 
 }
 
